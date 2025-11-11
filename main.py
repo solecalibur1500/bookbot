98fb35d80ff5dab92c82f780172e9ef09d02b3b6
solecalibur1500 <solecalibur1500@gmail.com>
2025-11-11 15:05:59 -0500

    bookbot pain

--- main.py
+++ main.py
@@ -1,10 +1,42 @@
-def get_book_text():
-    with open("books/frankenstein.txt") as f:
-        f_as_string = f.read()
-        return f_as_string
+import sys
+from stats import (
+    get_num_words,
+    chars_dict_to_sorted_list,
+    get_chars_dict,
+)
+
+
 def main():
-    text = get_book_text()
-    words = text.split()
-    print (f"Found {(len(words))} total words")    
-    return words
-main()
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
+    
+
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+    print_report(book_path, num_words, chars_sorted_list)
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+def print_report(book_path, num_words, chars_sorted_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"{item['char']}: {item['num']}")
+
+    print("============= END ===============")
+
+if __name__ == "__main__":
+    main()
